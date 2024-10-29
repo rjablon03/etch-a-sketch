21bc7a23307b460da5122817fc9cc938c13a8445
rjablon03 <secrets_crasser.0l@icloud.com>
2024-10-29 19:51:38 -0400

    Started working on logic

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-container = document.querySelector(".container");
+function renderGrid() {
+    const container = document.querySelector(".container");
+    const inputBox = document.querySelector(".text");
+    const tile = document.createElement("div");
+    tile.className = "tile";
 
+    for (let i = 0; i < parseInt(inputBox.textContent); i++) {
+        container.appendChild(tile);
+    }
+}
