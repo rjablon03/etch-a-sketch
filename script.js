4d85b6342e25a7e96d02bd057f3e143dfc7a02d3
rjablon03 <secrets_crasser.0l@icloud.com>
2024-11-01 11:47:13 -0400

    Started playing around with flexbox

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function createGrid() {
     const container = document.querySelector(".container");
+    let dimensions = document.querySelector(".dimensions");
+    dimensions = parseInt(dimensions.nodeValue);
+    console.log(dimensions)
 
     container.innerHTML = '';
     
-    for (let i = 0; i < 8*8; i++) {
+    for (let i = 0; i < 16*16; i++) {
         const tile = document.createElement("div");
         tile.classList.add("tile");
         container.appendChild(tile);
